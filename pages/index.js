bb9cb3b472d735b64bfa0abcddcf83a4276021db
Lennard Dorst <lennardsolana@gmail.com>
2023-03-05 16:12:14 +0100

    Styling.

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
       <div className='mt-[25vh] xl:mt-[30vh]'>
         <div className='p-5 xl:px-[50px] text-center'>
           <div className='flex flex-col items-center'>
-            <div className={headerClassName}>
+            <div className={headerClassName | ""}>
               <h1 className="text-4xl">Refine your prompts</h1>
               
               <p className="text-lg mt-2">Enter a prompt and refine it.</p>
@@ -71,7 +71,7 @@ export default function Home() {
 
             </div>
 
-            <div className={modalClassName}>
+            <div className={modalClassName | "hidden"}>
                 <div className="justify-end">
                   <button className="btn btn-square btn-sm justify-left" onClick={toggleView}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
@@ -84,7 +84,7 @@ export default function Home() {
                 </div>
               </div>
 
-              <div className={inputClassName}>
+              <div className={inputClassName | "form-control"}>
                 <div className="input-group">
                   <input type="text" placeholder="Enter prompt" className="input input-bordered" value={userPrompt} onChange={(e) => setUserPrompt(e.target.value) }/>
                   <button className="btn btn-square" onClick={refinePrompt}>
