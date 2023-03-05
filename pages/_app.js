8126baaa07922f09d13deb61c2cfe0f396f925f2
Lennard Dorst <lennardsolana@gmail.com>
2023-03-05 15:44:28 +0100

    Adding the theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <html data-theme="cupcake">
+    <Component {...pageProps} />
+  </html>
 }
