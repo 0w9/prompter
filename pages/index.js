19d220387f44a49941b4f973105cae582435a2c9
Lennard Dorst <lennardsolana@gmail.com>
2023-03-05 17:58:15 +0100

    Styling.

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,11 @@
-import Head from 'next/head'
-import { Inter, Modern_Antiqua } from '@next/font/google'
 import { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
-
   const [userPrompt, setUserPrompt] = useState('')
   const [refinedPrompt, setRefinedPrompt] = useState('')
-  const [modalClassName, setModalClassName] = useState('hidden')
-  const [inputClassName, setInputClassName] = useState('form-control')
-  const [headerClassName, setHeaderClassName] = useState('')
-
-  const toggleModal = () => {
-    if(modalClassName === 'hidden') {
-      setModalClassName('')
-    } else {
-      setModalClassName('')
-    }
-  }
-
-  const toggleInput = () => {
-    if(inputClassName === 'form-control hidden') {
-      setInputClassName('form-control')
-    } else {
-      setInputClassName('form-control hidden')
-    }
-  }
-
-  const toggleHeader = () => {
-    if(headerClassName === 'hidden') {
-      setHeaderClassName('')
-    } else {
-      setHeaderClassName('hidden')
-    }
-  }
+  const [inputIsShown, setInputIsShown] = useState(true)
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [headerIsShown, setIsShown] = useState(true);
 
   const refinePrompt = async () => {
     const response = await fetch('/api/generate', {
@@ -50,9 +21,9 @@ export default function Home() {
   }
 
   const toggleView = () => {
-    toggleModal()
-    toggleInput()
-    //toggleHeader()
+    setIsOpen(!modalIsOpen)
+    setIsShown(!headerIsShown)
+    setInputIsShown(!inputIsShown)
   }
 
   return (
@@ -60,31 +31,43 @@ export default function Home() {
       <div className='mt-[25vh] xl:mt-[30vh]'>
         <div className='p-5 xl:px-[50px] text-center'>
           <div className='flex flex-col items-center'>
-            <div className={headerClassName | ""}>
-              <h1 className="text-4xl">Refine your prompts</h1>
-              
-              <p className="text-lg mt-2">Enter a prompt and refine it.</p>
+            {
+              headerIsShown ? (
+                <div>
+                  <h1 className="text-4xl">Refine your prompts</h1>
+                  <p className="text-lg mt-2">Enter a prompt and refine it.</p>
 
-              <div className='mt-5'>
-                <div className="divider"></div>
-              </div>
-
-            </div>
-
-            <div className={modalClassName | "hidden"}>
-                <div className="justify-end">
-                  <button className="btn btn-square btn-sm justify-left" onClick={toggleView}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                  </button>
-                </div>
-
-                <div className="card-body text-left shadow-xl">
-                  <h2 className="card-title text-left">Prompt for your idea: {userPrompt}</h2>
-                  <p className="text-left">{refinedPrompt}</p>
+                <div className='mt-5'>
+                  <div className="divider"></div>
                 </div>
               </div>
+              ) : (
+                <></>
+              )
+            }
 
-              <div className={inputClassName | "form-control"}>
+            {
+              modalIsOpen ? (
+                <div>
+                  <div className="justify-end">
+                    <button className="btn btn-square btn-sm justify-left" onClick={toggleView}>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </button>
+                  </div>
+
+                  <div className="card-body text-left shadow-xl">
+                    <h2 className="card-title text-left">Prompt for your idea: {userPrompt}</h2>
+                    <p className="text-left">{refinedPrompt}</p>
+                  </div>
+                </div>
+              ) : (
+                <></>
+              )
+            }
+
+            {
+              inputIsShown ? (
+                <div className="form-control">
                 <div className="input-group">
                   <input type="text" placeholder="Enter prompt" className="input input-bordered" value={userPrompt} onChange={(e) => setUserPrompt(e.target.value) }/>
                   <button className="btn btn-square" onClick={refinePrompt}>
@@ -92,6 +75,10 @@ export default function Home() {
                   </button>
                 </div>
               </div>
+              ) : (
+                <></>
+              )
+            }
           </div>
         </div>
       </div>
